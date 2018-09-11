56de519281dd718476b8a71513545728829cd76c
BarZamsky <barzamsky1@gmail.com>
2018-09-11 20:52:12 +0300

    Add GET /users/me and authentication midleware

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //process.env.PROD_MONGODB
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://bar123:bar1234@ds151612.mlab.com:51612/todos1' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true} );
+//'mongodb://bar123:bar1234@ds151612.mlab.com:51612/todos1' ||
+mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true} );
 
 module.exports = {mongoose};
