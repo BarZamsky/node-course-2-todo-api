0de2cb946353522cfd73033c4b39a82ced338b56
BarZamsky <barzamsky1@gmail.com>
2018-09-11 14:18:49 +0300

    Setup seperate test and developments env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var url = 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
 //process.env.PROD_MONGODB
-mongoose.connect('mongodb://bar123:bar1234@ds151612.mlab.com:51612/todos1' || url, { useNewUrlParser: true });
+mongoose.connect('mongodb://bar123:bar1234@ds151612.mlab.com:51612/todos1', { useNewUrlParser: true });
 
 module.exports = {mongoose};
