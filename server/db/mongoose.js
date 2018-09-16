73b77803b54d0c0b89f896ab3e740a4b062a74a2
BarZamsky <barzamsky1@gmail.com>
2018-09-16 19:54:15 +0300

    change mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.set('useCreateIndex', true);
-// MONGODB_URI = 'mongodb://bar123:bar1234@ds151612.mlab.com:51612/todos1';
+MONGODB_URI = 'mongodb://bar123:bar1234@ds151612.mlab.com:51612/todos1';
 mongoose.connect( process.env.MONGODB_URI, { useNewUrlParser: true});
 //
 module.exports = {mongoose};
