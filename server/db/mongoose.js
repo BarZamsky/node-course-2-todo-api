8eb91b521f4b6ae434795809cefd78e661013807
BarZamsky <barzamsky1@gmail.com>
2018-09-27 00:16:20 +0300

    Add async/await methods to server.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true);
 MONGODB_URI = 'mongodb://bar123:bar1234@ds151612.mlab.com:51612/todos1';
 mongoose.connect( process.env.MONGODB_URI, { useNewUrlParser: true});
 //
